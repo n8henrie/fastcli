9049ce33b22b57db6c2bc1d559bc08b6033e9a32
Nathan Henrie <nate@n8henrie.com>
2019-11-30 11:07:22 -0700

    Add python 3.8 and deprecate 3.5

--- setup.py
+++ setup.py
@@ -42,7 +42,9 @@ setup(
     classifiers=[
         "Natural Language :: English",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5"
+        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.7"
+        "Programming Language :: Python :: 3.8"
     ],
     extras_require={
         "dev": dev_requirements
