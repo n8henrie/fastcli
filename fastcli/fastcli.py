3783ad74cb3bea04de3f2c5e21c00354438c8b76
Nathan Henrie <nate@n8henrie.com>
2019-12-19 08:00:21 -0700

    Update dependencies and lint for 3.6-3.8

--- fastcli/fastcli.py
+++ fastcli/fastcli.py
@@ -1,4 +1,4 @@
-"""fastcli.py :: Command line interface to fast.com by Netflix
+"""fastcli.py :: Command line interface to fast.com by Netflix.
 
 Usage:
     python3 -m fastcli
@@ -17,15 +17,16 @@ import typing
 
 logging.basicConfig(
     level=logging.WARNING,
-    format='%(asctime)s %(name)-12s %(lineno)d %(levelname)-8s %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
-    )
+    format="%(asctime)s %(name)-12s %(lineno)d %(levelname)-8s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
 
 logger_name = "{} :: {}".format(__file__, __name__)
 logger = logging.getLogger(logger_name)
 
 
-async def test_download_speed(session: aiohttp.ClientSession, url: str):
+async def test_download_speed(session: aiohttp.ClientSession, url: str) -> int:
+    """Count the amount of data successfully downloaded."""
     result = 0
     try:
         async with session.get(url) as resp:
@@ -41,45 +42,48 @@ async def test_download_speed(session: aiohttp.ClientSession, url: str):
         return result
 
 
-def find_from(text: str, start: str, until: str) -> str:
+def _find_from(text: str, start: str, until: str) -> str:
     start_find = text.find(start)
     start_length = len(start)
     end_find = text.find(until, start_find + start_length)
     if -1 in (start_find, end_find):
         return ""
     else:
-        return text[start_find + start_length:end_find]
+        start_idx = start_find + start_length
+        end_idx = end_find
+        return text[start_idx:end_idx]
 
 
-def get_token() -> str:
-    url = 'https://fast.com'
+def _get_token() -> str:
+    url = "https://fast.com"
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as r:
         resp = r.read().decode()
-    js = find_from(resp, '<script src="', '"')
+    js = _find_from(resp, '<script src="', '"')
 
     js_url = url + js
     js_req = urllib.request.Request(js_url)
     with urllib.request.urlopen(js_req) as jr:
         jsresp = jr.read().decode()
 
-    token = find_from(jsresp, 'token:"', '"')
+    token = _find_from(jsresp, 'token:"', '"')
     return token
 
 
-async def main(token: str = '', timeout: typing.Union[float, int] = 10.0, https:
-        bool = True, url_count: int = 3, verbosity: int = logging.WARNING):
+async def main(
+    token: str = "",
+    timeout: typing.Union[float, int] = 10.0,
+    https: bool = True,
+    url_count: int = 3,
+    verbosity: int = logging.WARNING,
+) -> float:
+    """Create coroutines for speedtest and return results."""
+    token = token or _get_token()
 
-    token = token or get_token()
-
-    params = {
-        'https': True,
-        'urlCount': 3,
-        'token': token
-        }
+    params = {"https": True, "urlCount": 3, "token": token}
     query_str = urllib.parse.urlencode({k: str(v) for k, v in params.items()})
 
-    api_base = 'https://api.fast.com'
+    api_base = "https://api.fast.com"
     url = f"{api_base}/netflix/speedtest/v2?{query_str}"
 
     req = urllib.request.Request(url)
@@ -90,29 +94,50 @@ async def main(token: str = '', timeout: typing.Union[float, int] = 10.0, https:
     start_time = time.time()
 
     async with aiohttp.ClientSession() as session:
-        coros = [test_download_speed(session, target['url'])
-                 for target in resp_json["targets"]]
-        download_futs = asyncio.gather(*coros)
-        await asyncio.wait_for(download_futs, timeout=timeout)
+        coros = [
+            test_download_speed(session, target["url"])
+            for target in resp_json["targets"]
+        ]
+        done, pending = await asyncio.wait(coros, timeout=timeout)
+        for task in pending:
+            task.cancel()
+            await task
 
     duration = time.time() - start_time
     logger.info("Run time: {:.2f} seconds".format(duration))
 
-    mb = sum(download_futs.result()) * 8 / 1024 / 1024
+    results = [task.result() for task in done | pending]
+    mb = sum(results) * 8 / 1024 / 1024
     return mb / duration
 
 
-def run(*, timeout: float = 30, verbosity = logging.WARNING) -> float:
+def run(*, timeout: float = 30, verbosity: int = logging.WARNING) -> float:
+    """Create eventloop and run main coroutine."""
     logging.info("Starting fastcli download speed test...")
     loop = asyncio.new_event_loop()
     speed = loop.run_until_complete(main(timeout=timeout, verbosity=verbosity))
+    loop.close()
     return speed
 
+
 def cli() -> None:
-    parser = argparse.ArgumentParser(prog='fastcli',
-                                     argument_default=argparse.SUPPRESS)
-    parser.add_argument('--timeout', default=30, type=float,
-                        help="Duration of time to run speed test")
+    """Parse CLI arguments."""
+    parser = argparse.ArgumentParser(
+        prog="fastcli", argument_default=argparse.SUPPRESS
+    )
+    parser.add_argument(
+        "--timeout",
+        default=30,
+        type=float,
+        help="Duration of time to run speed test",
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=0,
+        action="count",
+        help="increase verbosity (may repeat up to -vvv)",
+    )
     namespace = parser.parse_args()
     args = {k: v for k, v in vars(namespace).items() if v}
     speed = run(**args)
--- tests/test_fastcli.py
+++ tests/test_fastcli.py
@@ -6,19 +6,23 @@ import urllib.request
 from fastcli import fastcli
 
 
-def test_find_from():
-    """Test the hacky `find_from` function
+def test_find_from() -> None:
+    """Test the hacky `_find_from` function
 
     Should return the first portion of `text` between `start` and `until`
     """
-    assert fastcli.find_from(text='<script src="/12345.js"',
-                             start='<script src="', until='"') == '/12345.js'
+    assert (
+        fastcli._find_from(
+            text='<script src="/12345.js"', start='<script src="', until='"'
+        )
+        == "/12345.js"
+    )
 
 
-def internet_is_on():
+def internet_is_on() -> bool:
     """Utility function to try to verify an internet connection exists"""
     headers = {"User-Agent": "fastcli"}
-    req = urllib.request.Request('https://n8henrie.com', headers=headers)
+    req = urllib.request.Request("https://n8henrie.com", headers=headers)
 
     try:
         with urllib.request.urlopen(req) as r:
@@ -26,13 +30,13 @@ def internet_is_on():
     except urllib.error.URLError:
         resp = {}
 
-    if resp.get('Date'):
+    if resp.get("Date"):
         return True
     else:
         return False
 
 
-def test_fastcli():
+def test_fastcli() -> None:
     """If an internet connect exists, it should have a measurable speed > 0"""
     if internet_is_on():
         assert fastcli.run(timeout=1) > 0
