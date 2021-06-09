7788d7f6a21d382f651f312869feaaad4d0f9ddc
Nathan Henrie <nate@n8henrie.com>
2021-06-09 15:27:49 -0600

    Lint with black

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
 import re
-from setuptools import setup, find_packages
 
-with open('README.md') as readme_file, open('HISTORY.md') as history_file:
+from setuptools import find_packages, setup
+
+with open("README.md") as readme_file, open("HISTORY.md") as history_file:
     readme = readme_file.read()
     history = history_file.read()
 
-with open('requirements.txt') as requirements_file:
+with open("requirements.txt") as requirements_file:
     requirements = requirements_file.read().splitlines()
 
-with open('requirements-dev.txt') as dev_requirements_file:
+with open("requirements-dev.txt") as dev_requirements_file:
     dev_requirements = dev_requirements_file.read().splitlines()
 
-version_regex = re.compile(r'__version__ = [\'\"]((\d+\.?)+)[\'\"]')
-with open('fastcli/__init__.py') as f:
+version_regex = re.compile(r"__version__ = [\'\"]((\d+\.?)+)[\'\"]")
+with open("fastcli/__init__.py") as f:
     vlines = f.readlines()
-__version__ = next(re.match(version_regex, line).group(1) for line in vlines
-                   if re.match(version_regex, line))
+__version__ = next(
+    re.match(version_regex, line).group(1)
+    for line in vlines
+    if re.match(version_regex, line)
+)
 
 setup(
     name="fastcli",
@@ -28,9 +32,7 @@ setup(
     url="https://github.com/n8henrie/fastcli",
     packages=find_packages(),
     include_package_data=True,
-    entry_points={
-        'console_scripts': ['fastcli=fastcli.fastcli:cli']
-        },
+    entry_points={"console_scripts": ["fastcli=fastcli.fastcli:cli"]},
     install_requires=requirements,
     license="MIT",
     zip_safe=False,
@@ -42,9 +44,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
     ],
-    extras_require={
-        "dev": dev_requirements
-        },
+    extras_require={"dev": dev_requirements},
     test_suite="tests",
-    tests_require=['pytest==6.2.4']
+    tests_require=["pytest==6.2.4"],
 )
